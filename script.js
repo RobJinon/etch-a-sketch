1bcb0c7f2529f0c4732f8ffe44a23c5cef4bf09f
Rob <jejinon@up.edu.ph>
2023-03-28 12:43:29 +0800

    Changed grid from tailwind to vanilla css; Added custom grid size feature, erase feature.

--- script.js
+++ script.js
@@ -9,12 +9,42 @@ let mouseDown = false
 document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
 
+var slider = $("#slider");
+var sliderValue = $("#slider_value");
+sliderValue.text(slider.val() + "x" + slider .val());
+
+slider.on("input", function() {
+    sliderValue.text($(this).val() + "x" + $(this).val());
+    grid.empty();
+    setupGrid($(this).val());
+});
+
+var colorMode = "";
+
 function changeColor(e){
     if (e.type === 'mouseover' && !mouseDown){
         return
     }
-    e.target.classList.add('bg-slate-600');
+    if (colorMode === 'erase'){
+        e.target.classList.remove('bg-slate-800');
+        e.target.classList.add('bg-white');
+    }
+    else{
+        e.target.classList.remove('bg-white');
+        e.target.classList.add('bg-slate-800');
+    }
+    
 }
+
+$('#erase').click(function(){
+    colorMode = 'erase';
+});
+
+$('#black').click(function(){
+    colorMode = '';
+});
+
+
 
 function setupGrid(size){
     grid.addClass('grid-cols-'+size);
@@ -22,16 +52,22 @@ function setupGrid(size){
 
     for(var i = 0; i < size*size; i++){
         const gridElement = document.createElement('div');
-        gridElement.classList.add('cell', 'border', 'border-slate-800');
+        gridElement.classList.add('cell');
 
         gridElement.addEventListener('mouseover', changeColor);
         gridElement.addEventListener('mousedown', changeColor);
         $('#grid').append(gridElement);
     }
+
+    grid.css({
+        "grid-template-columns": "repeat(" + size + ", 1fr)",
+        "grid-template-rows": "repeat(" + size + ", 1fr)"
+    });
 }
 
-setupGrid(6);
+setupGrid(16);
 
 $('#clear').click(function(){
-    $('.cell').removeClass('bg-slate-600');
+    grid.empty();
+    setupGrid(slider.val());
 });
